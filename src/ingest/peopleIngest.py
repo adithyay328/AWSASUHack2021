fb9ae4085b4080c04f7b8fec81dbcabf78887c47
Adithya Yerramsetty <adithyay@hotmail.com>
2021-11-06 21:30:03 -0700

    Added basic code to ingest a CSV full of people.

---
+++ src/ingest/peopleIngest.py
@@ -0,0 +1,27 @@
+from os import curdir
+import psycopg2
+
+import csv
+
+conn = psycopg2.connect("dbname=covardb user=covardbapps")
+cur = conn.cursor()
+
+with open("example.csv", newline='') as csvFile:
+    cReader = csv.reader(csvFile, delimiter=',')
+    for row in cReader:
+        print(','.join(row))
+
+        infected = row[0] == "true"
+        uid = row[1]
+        daysInfected = int(row[2])
+        age = int(row[3])
+        medicalCond = int(row[4])
+
+        cur.execute("INSERT INTO people(uid, infected, dayssinceinfection, age, medicalconditions) VALUES (%s, %s, %s, %s, %s)", (
+            uid, infected, daysInfected, age, medicalCond
+        ))
+
+        conn.commit()
+
+cur.close()
+conn.close()
